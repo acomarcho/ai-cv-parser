51845bfedf3ea8e842378d0be906106ee9528b5f
acomarcho <acoxstpd@gmail.com>
2025-02-20 20:18:34 +0700

    feat: prepare styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
 export default function Home() {
   return (
-    <div>
-      <p>Hello, world!</p>
+    <div className="p-8">
+      <h1 className="text-4xl text-stone-800 font-bold">AI CV Parser</h1>
+      <p className="text-stone-500 text-lg mt-4">
+        Parse CVs easily with the help of AI!
+      </p>
+      <p className="text-stone-500 text-lg">
+        We will parse your CV and extract the information for you.
+      </p>
     </div>
   );
 }
